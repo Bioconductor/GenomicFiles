4e2364b6eaea70e24ae26b705959821dd1cc014d
Martin Morgan <mtmorgan@fhcrc.org>
2016-01-23 21:07:21 +0000

    cleanup: unit tests need Rsamtools NAMESPACE
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicFiles@112832 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_reduceByFile-methods.R
+++ inst/unitTests/test_reduceByFile-methods.R
@@ -2,9 +2,9 @@ fl <- system.file("extdata", "ex1.bam", package="Rsamtools")
 gr <- GRanges(c("seq1", "seq2", "seq2"), IRanges(1:3, width=100))
 gf <- GenomicFiles(gr, c(one=fl, two=fl))
 
-MAP = function(RANGE, FILE, ..., param=ScanBamParam()) {
-    bamWhich(param) <- RANGE 
-    countBam(FILE, param=param)
+MAP = function(RANGE, FILE, ..., param=Rsamtools::ScanBamParam()) {
+    Rsamtools::bamWhich(param) <- RANGE 
+    Rsamtools::countBam(FILE, param=param)
 }
 
 REDUCE = function(MAPPED, ...) {
@@ -29,7 +29,7 @@ test_reduceByFile_MAP <- function()
     checkIdentical(dim(assays(ans)$data), c(3L, 2L))
 }
  
- test_reduceByFile_MAP_REDUCE <- function()
+test_reduceByFile_MAP_REDUCE <- function()
 {
     ans <- reduceByFile(gf, MAP=MAP, REDUCE=REDUCE)
     checkIdentical(length(ans), 2L)
@@ -42,7 +42,7 @@ test_reduceFiles_MAP <- function()
     ## No REDUCE applied, MAP returns 
     ans0 <- reduceFiles(gf, MAP=MAP)
     checkIdentical(length(ans0), 2L)
-    checkIdentical(unname(elementLengths(ans0)), c(1L, 1L))
+    checkIdentical(unname(lengths(ans0)), c(1L, 1L))
     elts <- lapply(ans0, elementLengths)
     checkIdentical(names(elts), c("one", "two"))
     checkIdentical(unlist(elts, use.names=FALSE), c(3L, 3L))
@@ -50,10 +50,10 @@ test_reduceFiles_MAP <- function()
     ans1 <- reduceFiles(gr, c(one=fl, two=fl), MAP=MAP)
     checkIdentical(ans0, ans1)
     checkIdentical(length(ans1), 2L)
-    checkIdentical(unname(elementLengths(ans1)), c(1L, 1L))
+    checkIdentical(unname(lengths(ans1)), c(1L, 1L))
 }
 
- test_reduceFiles_MAP_REDUCE <- function()
+test_reduceFiles_MAP_REDUCE <- function()
 {
     ## REDUCE applied single time after MAP, simply unlist
     REDUCE <- function(mapped, ...) do.call(rbind, mapped)
--- inst/unitTests/test_reduceByRange-methods.R
+++ inst/unitTests/test_reduceByRange-methods.R
@@ -2,9 +2,9 @@ fl <- system.file("extdata", "ex1.bam", package="Rsamtools")
 gr <- GRanges(c("seq1", "seq2", "seq2"), IRanges(1:3, width=100))
 gf <- GenomicFiles(gr, c(one=fl, two=fl))
 
-MAP = function(RANGE, FILE, ..., param=ScanBamParam()) {
-    bamWhich(param) <- RANGE 
-    countBam(FILE, param=param)
+MAP = function(RANGE, FILE, ..., param=Rsamtools::ScanBamParam()) {
+    Rsamtools::bamWhich(param) <- RANGE 
+    Rsamtools::countBam(FILE, param=param)
 }
 
 REDUCE = function(MAPPED, ...) {
@@ -16,11 +16,11 @@ test_reduceByRange_MAP <- function()
 {
     ans <- reduceByRange(gf, MAP=MAP)
     checkIdentical(length(ans), 3L)
-    checkIdentical(unname(elementLengths(ans)), c(2L, 2L, 2L))
+    checkIdentical(unname(lengths(ans)), c(2L, 2L, 2L))
 
     ans <- reduceByRange(gr, c(one=fl, two=fl), MAP=MAP)
     checkIdentical(length(ans), 3L)
-    checkIdentical(unname(elementLengths(ans)), c(2L, 2L, 2L))
+    checkIdentical(unname(lengths(ans)), c(2L, 2L, 2L))
 
     ## summarize = TRUE
     ans <- reduceByRange(gf, MAP=MAP, summarize=TRUE)
